68c5caffc7294cc0aacea99270e5f54c0add5444
edwardliucong <edwardliucong@didichuxing.com>
2018-10-17 15:48:24 +0800

    update

--- src/main/java/com/example/shelter/controller/ShelterSimpleTraceController.java
+++ src/main/java/com/example/shelter/controller/ShelterSimpleTraceController.java
@@ -11,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShelterSimpleTraceController {
 
 
+    /**
+     * 主要用来检测用户真正的检索标签
+     *
+     * 往往用户的需求在不断的使用中才会真正表现出来
+     */
     public void watchQueryResourceInfo(){
 
         // 记录查询资源列表时的检索条件
+
+        //
     }
 
 }
